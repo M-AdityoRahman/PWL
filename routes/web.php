fe821a1add3ec24a23b1bb02c9fcde206890710c
M-AdityoRahman <adityorahman18@gmail.com>
2025-03-06 22:16:24 +0700

    Jobsheet2_Praktikum3_no.9

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,4 @@ Route::resource('photos', PhotoController::class);
 
 Route::resource('photos', PhotoController::class)->only(['index', 'show']);
 Route::resource('photos', PhotoController::class)->except(['create', 'store', 'update', 'destroy']);
-Route::get('/greeting', function () {return view('blog.hello', ['name' => 'Adityo']);});
+Route::get('/greeting', [WelcomeController::class,'greeting']);
