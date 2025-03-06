7775d6ceaa6cdfe210fe465736b6f44c05ec6423
M-AdityoRahman <adityorahman18@gmail.com>
2025-03-06 21:17:41 +0700

    Jobsheet2_Praktikum1_no.13

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/posts/{post}/comments/{comment}', function
-($postId, $commentId) {
-    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+Route::get('/route/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' .$id;
 
 });
