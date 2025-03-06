3b7726592425d4c31d37371b86c6a7f5ee20819a
M-AdityoRahman <adityorahman18@gmail.com>
2025-03-06 21:57:51 +0700

    Jobsheet2_Praktikum2_no.7

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +18,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/hello', [WelcomeController::class,'hello']);
-Route::get('/', [WelcomeController::class,'index']);
-Route::get('/about', [WelcomeController::class,'about']);
-Route::get('/articles/{id?}', [WelcomeController::class,'articles']);
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'index']);
+Route::get('/articles/{id?}', [ArticleController::class, 'show']);
