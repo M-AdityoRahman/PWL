4238bf18b8483dfea8e55152956341093baec91d
M-AdityoRahman <adityorahman18@gmail.com>
2025-03-06 21:19:10 +0700

    Jobsheet2_Praktikum1_no.14

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/route/articles/{id}', function ($id) {
-    return 'Halaman Artikel dengan ID ' .$id;
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama Saya ' .$name;
 
 });
